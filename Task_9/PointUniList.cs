f5a237cb3f122ec3aa870de6a13f2c2f619a23dd
Андрей Филимонов <aafilimonov_4@edu.hse.ru>
2020-06-12 16:24:04 +0500

    Добавьте файлы проекта.

---
+++ Task_9/PointUniList.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_9
+{
+    class PointUniList
+    {
+        // Информационное поле
+        public int data;
+
+        // Адресное поле
+        public PointUniList next;
+
+        // Конструктор с параметром
+        public PointUniList(int N)
+        {
+            data = N;
+            next = null;
+        }
+
+        public override string ToString()
+        {
+            return data + " ";
+        }
+
+        // Создание элемента списка
+        static PointUniList MakePoint(int N)
+        {
+            PointUniList p = new PointUniList(N);
+            return p;
+        }
+
+        // Добавление в начало однонаправленного списка
+        public static PointUniList MakeList(int N, PointUniList beg = null)
+        {
+            // Создаем элемент и добавляем в начало списка
+            PointUniList p = MakePoint(N);
+            p.next = beg;
+            beg = p;
+
+            if (N <= 1)
+            {
+                return beg;
+            }
+
+            return MakeList(--N, beg);
+        }
+
+        public static void Print(PointUniList beg)
+        {
+            // Проверка наличия элементов в списке
+            if (beg == null)
+            {
+                Console.WriteLine("Список пустой");
+                return;
+            }
+
+            PointUniList p = beg;
+
+            Console.WriteLine("Список:");
+            while (p != null)
+            {
+                Console.WriteLine(p.ToString());
+                // Переход к следующему элементу
+                p = p.next;
+            }
+        }
+
+        public static PointUniList DelElement(PointUniList beg, int number, PointUniList p = null, int index = 0)
+        {
+            if (beg == null)
+            {
+                Console.WriteLine("Удалять нечего: список пуст");
+                return beg;
+            }
+
+            if (number == 1)
+            {
+                beg = beg.next;
+                return beg;
+            }
+
+            if (p == null)
+            {
+                if (index == 0)
+                {
+                    p = beg;
+                }
+                else
+                {
+                    return beg;
+                }
+            }
+
+            if (p.next?.data == number)
+            {
+                p.next = p.next.next;
+                return beg;
+            }
+
+            return DelElement(beg, number, p.next, ++index);
+        }
+
+        public static int FindElement(PointUniList beg, int findNumber, PointUniList p = null, int index = 0)
+        {
+            if (beg == null)
+            {
+                Console.WriteLine("Искать нечего: список пуст");
+                return -1;
+            }
+
+            if (p == null)
+            {
+                if (index == 0)
+                {
+                    p = beg;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            if (p.data == findNumber)
+            {
+                return p.data;
+            }
+
+            return FindElement(beg, findNumber, p.next, ++index);
+        }
+    }
+}
---
+++ Task_9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во элементов списка:");
+            int N = InputInt(1, 100);
+            PointUniList list = PointUniList.MakeList(N);
+            PointUniList.Print(list);
+            Console.WriteLine("Введите элемент для удаления:");
+            int numForDel = InputInt(1, 100);
+            list = PointUniList.DelElement(list, numForDel);
+            PointUniList.Print(list);
+            Console.WriteLine("Введите элемент для поиска:");
+            int numForFind = InputInt(1, 100);
+            int element = PointUniList.FindElement(list, numForFind);
+            Console.WriteLine(element == -1 ? "Элемент не найден" : "Найденный элемент: " + element);
+
+            Console.ReadLine();
+        }
+        public static int InputInt(int min, int max)
+        {
+            int num;
+
+            while (!int.TryParse(Console.ReadLine(), out num) || num < min || num > max)
+            {
+                if (num < min || num > max)
+                {
+                    Console.WriteLine($"Введите целое число от {min} до {max}");
+                    continue;
+                }
+
+                Console.WriteLine("Введите целое число");
+            }
+
+            return num;
+        }
+    }
+}
